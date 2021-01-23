82ad1e544b2bcbaf354c783610ca4ea72ec3c649
Pete <pavlovskipetko@abv.bg>
2021-01-23 22:06:09 +0200

    remove dummy html

--- src/test/kotlin/com/coffeesupply/CoffeeResourceTest.kt
+++ src/test/kotlin/com/coffeesupply/CoffeeResourceTest.kt
@@ -2,7 +2,6 @@ package com.coffeesupply
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
